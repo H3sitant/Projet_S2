dd2d371ae547f2c8cfbb2122bffbb29d1fe96d9b
Victor <vicfreve@gmail.com>
2020-02-19 13:56:08 -0500

    Merge remote-tracking branch 'origin/master'

--- SkyBurger/Personnage.h
+++ SkyBurger/Personnage.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Condiment.h"
+
 #include <stack>
 using namespace std;
 class Personnage
@@ -8,11 +9,13 @@ private:
 	int position;
 	stack<Condiment> condiments;
 public:
+	Personnage();
+	~Personnage();
 	int getPosition();
-	void setPosition(int position);
+	void setPosition(int newPosition);
 	stack<Condiment> getCondiments();
 	void setCondiments(stack<Condiment> condiments);
-	void deplacer(int direction);
+	void deplacer(int direction, int largeur);
 	int getHauteur();
 };
 
--- SkyBurger/VueJeux.h
+++ SkyBurger/VueJeux.h
@@ -1,5 +1,15 @@
 #pragma once
 class VueJeux
 {
+public:
+	VueJeux();
+	~VueJeux();
+	int getHauteur();
+	void setHauteur(int pos);
+	int getLargeur();
+	void setLargeur(int pos);
+private:
+	int Largeur;
+	int Hauteur;
 };
 
