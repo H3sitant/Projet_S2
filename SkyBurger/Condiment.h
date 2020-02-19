18b3f3eb17f11d5e7c39270eb539d9b059e31ec7
Victor <vicfreve@gmail.com>
2020-02-19 13:46:22 -0500

    condiment.h

--- SkyBurger/Condiment.h
+++ SkyBurger/Condiment.h
@@ -6,7 +6,19 @@ class Condiment
 public:
 	enum SorteCondiment{LAITUE, PAIN, TOMATE, OIGNON, BOULETTE};
 	ostream& afficher(ostream& out);
-private:
+	friend ostream& operator<<(ostream& out, Condiment c);
+	SorteCondiment getSorte();
+	void setSorte(SorteCondiment sorte);
+	Point getPosition();
+	void setPosition(Point Position);
 
+private:
+	SorteCondiment sorte;
+	Point position;
+};
+
+struct Point {
+	int x;
+	int y;
 };
 
