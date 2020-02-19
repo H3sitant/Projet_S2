e459236428aeb9371d9b805bb25da8a7e558edce
Victor <vicfreve@gmail.com>
2020-02-19 13:26:03 -0500

    Condiment et personnage.h

--- SkyBurger/Condiment.h
+++ SkyBurger/Condiment.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <ostream>
+using namespace std;
 class Condiment
 {
+public:
+	enum SorteCondiment{LAITUE, PAIN, TOMATE, OIGNON, BOULETTE};
+	ostream& afficher(ostream& out);
+private:
+
 };
 
--- SkyBurger/Personnage.h
+++ SkyBurger/Personnage.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "Condiment.h"
+#include <stack>
+using namespace std;
 class Personnage
 {
+private:
+	int position;
+	stack<Condiment> condiments;
+public:
+	int getPosition();
+	void setPosition(int position);
+	stack<Condiment> getCondiments();
+	void setCondiments(stack<Condiment> condiments);
+	void deplacer(int direction);
+	int getHauteur();
 };
 
