1ed2d45646ca35c8c6a58c3c08853a7fbb827f72
Isack Breton <isackbreton@live.ca>
2020-02-19 13:23:54 -0500

    CTRJeu

--- SkyBurger/CtrlrJeux.cpp
+++ SkyBurger/CtrlrJeux.cpp
@@ -1,1 +1,51 @@
 #include "CtrlrJeux.h"
+#define droite 1
+#define gauche 0
+
+CtrlrJeux::CtrlrJeux()
+{
+	int hauteur = vue.getHauteur();
+	int largeur = vue.getLargeur();
+	//char afficher[hauteur][largeur];
+}
+CtrlrJeux::~CtrlrJeux()
+{
+
+}
+void CtrlrJeux:: ActionDeplacer(int direction)
+{
+	int position = personnage.getPosition();
+	int largeur = vue.getLargeur();
+	if (direction == gauche)
+	{
+		if (position == 0)
+		{
+			position = largeur;
+		}
+		else
+		{
+			position--;
+		}
+	}
+	else
+	{
+		if (position == largeur)
+		{
+			position = 0;
+		}
+		else
+		{
+			position++;
+		}
+	}
+}
+
+void CtrlrJeux::Pause()
+{
+	
+}
+void CtrlrJeux ::Afficher()
+{
+
+
+}
