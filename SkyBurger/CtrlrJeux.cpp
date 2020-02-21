e75ce63053a169bf34dc2a3ce9edce522914efba
Victor <vicfreve@gmail.com>
2020-02-20 20:37:42 -0500

    Corrections merge

--- SkyBurger/CtrlrJeux.cpp
+++ SkyBurger/CtrlrJeux.cpp
@@ -25,60 +25,61 @@ void CtrlrJeux::Pause()
 	
 }
 //Affichage � la console chaque �l�ment en jeux
-const void CtrlrJeux ::Afficher()
+const void CtrlrJeux::Afficher()
 {
 	char item;
-		system("cls");
-		Personnage cpyPersonnage = personnage.copy();
-		vector<Condiment> cpyFalling(fallingCondiments);
-		for (int i = hauteur - 1; i >= 0; i--) 
+	system("cls");
+	Personnage cpyPersonnage = personnage.copy();
+	list<Condiment*> cpyFalling(fallingCondiments);
+	for (int i = hauteur - 1; i >= 0; i--)
+	{
+		bool objetL = false;
+		for (Condiment* c2 : cpyFalling)
 		{
-			bool objetL =false;
-			for (Condiment c2 : cpyFalling)
-			{
-				cout << c2.getPosition().y << endl;
-				if (c2.getPosition().y == i)
-				{
-					objetL = true;
-					break;
-				}
-			}
-			if (i == 0)
+			cout << c2->getPosition().y << endl;
+			if (c2->getPosition().y == i)
 			{
 				objetL = true;
+				break;
 			}
-			if (objetL == true)
+		}
+		if (i == 0)
+		{
+			objetL = true;
+		}
+		if (objetL == true)
+		{
+			for (int j = 0; j < largeur; j++)
 			{
-				for (int j = 0; j < largeur; j++) 
+				item = ' ';
+				for (Condiment* c2 : cpyFalling)
 				{
-					item = ' ';
-					for (Condiment c2 : cpyFalling) 
+					if (c2->getPosition().y == i && c2->getPosition().x == j)
 					{
-						if (c2.getPosition().y == i && c2.getPosition().x == j) 
-						{
-							item = c2.toString();
-							break;
-						}
+						item = c2->toString();
+						break;
 					}
+				}
 
-					if (i == 0 && j == cpyPersonnage.getPosition()) 
-					{
+				if (i == 0 && j == cpyPersonnage.getPosition())
+				{
 
-						item = cpyPersonnage.toString();
-					}
+					item = cpyPersonnage.toString();
+				}
 
 				for (Condiment* c : cpyPersonnage.getCondiments()) {
 					if (c->getPosition().y == i && c->getPosition().x == j) {
 						item = c->toString();
 						break;
 					}
-					cout << item;
 				}
+				cout << item;
 			}
-			cout << endl;
 		}
-		cout << endl << endl;
+		cout << endl;
 	}
+	cout << endl << endl;
+}
 bool CtrlrJeux::getActif()
 {
 	return Jeu_Actif;
